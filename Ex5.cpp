6ea59d36fd08950d82f1800ca11a22bf053a850d
ritesh4990 <riteshpatil3635@gmail.com>
2026-05-04 00:07:55 +0530

    Add files via upload

---
+++ Ex5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    static int count; 
+public:
+    Student() {
+        count++; 
+    }
+    static void showCount() {
+        cout << "Total number of students: " << count << endl;
+    }
+};
+
+// Initialization of static data member
+int Student::count = 0;
+
+int main() {
+    Student s1;
+    Student s2;
+    Student s3;
+    
+    Student::showCount();
+    return 0;
+}
